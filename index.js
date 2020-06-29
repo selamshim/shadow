3abb70b2123e7102d051d34ece6b49c6275fbaa1
selamshimelis <shimeselam@gmail.com>
2020-06-29 22:38:13 +0300

    shadow edit

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ function shadow(options){
     options.shadow_type= '0px'
 
     else
-    options.shadow_type=='15px'
+    options.shadow_type= '15px'
 
     image.forEach(image=>{
         image.style.boxshadow=`10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
 
         if(options.padding){
-            image.style.padding='1em';
+            image.style.padding= '1em';
         }
     })
 }
